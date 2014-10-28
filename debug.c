47fdffd59ff8f95954414dfec804c7151d6f24cb
Vikram Datt Rana <vrana@usc.edu>
2014-10-28 14:28:15 -0700

    ver 1.0.0
    Just adding some proper functions to the file

--- debug.c
+++ debug.c
@@ -1,6 +1,46 @@
+/*
+ * ============================================================================
+ * File Name: debug.c
+ *
+ * Description:
+ *
+ * Revision History:
+ * ============================================================================
+ */
 #include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include "debug.h"
 
-int main(int argc, char ** argv)
+#ifdef DEBUG
+/*
+ * ============================================================================
+ * Function Name: DBG_TRACE
+ *
+ * Description	:
+ *
+ * Input Params	:
+ *
+ * Output Params:
+ * ============================================================================
+ */
+void DBG_TRACE(char * szFmt, ...)
 {
-  return 0;
+		char	szLocFmt[256]	= "";
+		va_list	arguments;
+
+		va_start(arguments, szFmt);
+
+		sprintf(szLocFmt, "\n%s", szFmt);
+		vfprintf(stderr, szLocFmt, arguments);
+
+		va_end(arguments);
 }
+#endif
+
+/*
+ * ============================================================================
+ * End of file debug.c
+ * ============================================================================
+ */
