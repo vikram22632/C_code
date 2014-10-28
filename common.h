84031b58dcc13377d65d5cd1bd2c524f060d54f9
Vikram Datt Rana <vrana@usc.edu>
2014-10-28 16:20:45 -0700

    ver 0.00
    This header file is a common file containing common data used for all purposes

---
+++ common.h
@@ -0,0 +1,18 @@
+#ifndef __COMMON_HEADER
+#define __COMMON_HEADER
+
+#define SUCCESS	0
+#define FAILURE	-1444
+
+typedef enum
+{
+	false = 0,
+	true
+}
+VBOOL;
+
+typedef unsigned int	uint;
+typdeef unsigned long	ulong;
+typedef void *			any_t;
+
+#endif
