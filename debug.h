84ed101425073a33bcbe9e208bacf493c4348475
Vikram Datt Rana <vrana@usc.edu>
2014-11-04 12:33:56 -0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -2,9 +2,13 @@
 #define __DBG_HEADER
 
 #ifdef DEBUG
+extern int initDebug(char *);
 extern void DBG_TRACE(char *, ...);
+extern void TDBG_TRACE(char *, ...);
 #else
+#define initDebug(0);
 #define DBG_TRACE(...);
+#define TDBG_TRACE(...);
 #endif
 
 #endif
