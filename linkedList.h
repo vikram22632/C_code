200e4b3c386c9ed863552eb691cc82972c1aea34
Vikram Datt Rana <vrana@usc.edu>
2014-11-04 20:47:13 -0800

    Update linkedList.h
    Adding the linked list node structure definition

--- linkedList.h
+++ linkedList.h
@@ -1,4 +1,13 @@
 #ifndef __LKD_LIST_HEADER
 #define __LKD_LIST_HEADER
 
+#include "common.h"
+
+typedef struct _llNode
+{
+    struct _llNode *    next;
+    struct _llNode *    prev;
+    any_t               data;
+}LLNODE_STYPE, * LLNODE_PTYPE;
+
 #endif
