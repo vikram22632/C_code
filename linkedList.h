31a4605dd67e29a3bedb67330af7101c5a7600c2
Vikram Datt Rana <vrana@usc.edu>
2014-11-05 02:23:51 -0800

    ver 1.0.0
    Added function declarations and the linked list controller structure definition

--- linkedList.h
+++ linkedList.h
@@ -3,11 +3,42 @@
 
 #include "common.h"
 
-typedef struct _llNode
+typedef enum
 {
-    struct _llNode *    next;
-    struct _llNode *    prev;
-    any_t               data;
-}LLNODE_STYPE, * LLNODE_PTYPE;
+	BUBBLE_SORT = 1,
+	INSERT_SORT,
+	MERGE_SORT,
+	SELECT_SORT,
+	QUICK_SORT
+}
+LL_SORT_TYPE;
+
+typedef VBOOL (*lldata_compFunc)(any_t, any_t);
+
+typedef struct _llist
+{
+	int		elemCnt;
+	any_t	anchor;
+}
+LIST_STYPE, * LIST_PTYPE;
+
+extern int listInit(LIST_PTYPE);
+
+extern int list_length(LIST_PTYPE list);
+extern VBOOL list_empty(LIST_PTYPE list);
+
+extern int list_addAtBeg(LIST_PTYPE list, any_t data);
+extern int list_addAtEnd(LIST_PTYPE list, any_t data);
+extern int list_insertafter(LIST_PTYPE list, any_t data, any_t llnode);
+extern int list_insertbefore(LIST_PTYPE list,any_t data, any_t llnode);
+extern void list_unlink(LIST_PTYPE list, any_t llnode);
+extern void list_unlinkall(LIST_PTYPE list);
+
+extern any_t list_first(LIST_PTYPE list);
+extern any_t list_last(LIST_PTYPE list);
+extern any_t list_next(LIST_PTYPE list, any_t llnode);
+extern any_t list_prev(LIST_PTYPE list, any_t llnode);
+
+extern any_t list_find(LIST_PTYPE list, any_t data);
 
 #endif
